638caf798d6ae9aedbbe27eadf15a467fbf1fe61
dario1899 <dario1899>
2026-04-18 16:27:48 +0200

    cloud run api

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://v1-versusy-928473098122.europe-west1.run.app',
         changeOrigin: true,
       },
     },
